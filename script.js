7d9877c9243e45b7ed0a1fac0c31aaed1b0ff450
Elden <eldendeguia@outlook.com>
2024-08-20 08:49:35 -0500

    Added function getComputerChoice
    This functions generates a random number then
    converts it to a choice in the game.

---
+++ script.js
@@ -0,0 +1,29 @@
+// Goal: Function to generate random computer choice
+// Steps: (1) Generate a random number between 0-2, 
+// (2) Use switch statement to turn this number into an option
+
+function getComputerChoice() {
+    // Step 1:
+    let randomNumber = Math.floor(Math.random() * 3);
+    
+    // Step 2:
+    let choice;
+
+    switch (randomNumber) {
+        case 0:
+            choice = "Rock";
+            break;
+
+        case 1:
+            choice = "Paper"
+            break;
+
+        case 2:
+            choice = "Scissors"
+            break;
+    }
+
+    return choice;
+}
+
+console.log(getComputerChoice());
