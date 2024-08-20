ef3acb1e5bafd3aa5a2aa33e766b8b0b11501540
Elden <eldendeguia@outlook.com>
2024-08-20 09:28:28 -0500

    Finished function playGame
    This function calls playRound five times
    and displays the winner to the console based on
    score.

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 // Goal: Function to generate random computer choice
 // Steps: (1) Generate a random number between 0-2, 
 // (2) Use switch statement to turn this number into an option
@@ -36,63 +33,85 @@ function getHumanChoice() {
     return choice;
 }
 
-// Goal: Function to play a round
-// Steps: (1) Make choices case-insensitive (2) Decide winner
-// using conditionals (3) Print proper message
+// Goal: Function to play five rounds and declare a winner
+// Step 1: Call playRound 5 times Step 2: Decide winner and
+// print proper message based on score
 
-function playRound(humanChoice, computerChoice) {
-    // Step 1:
-    humanChoice = humanChoice.slice(0,1).toUpperCase() + humanChoice.substr(1).toLowerCase();
-    computerChoice = computerChoice.slice(0,1).toUpperCase() + computerChoice.substr(1).toLowerCase();
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-    // Step 2/3:
-    if (humanChoice == "Rock") {
-        if (computerChoice == "Rock") {
-            console.log("It's a tie!");
-        }
-
-        else if (computerChoice == "Paper") {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-            computerScore++;
-        }
-
-        else {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-            humanScore++;
-        }
+    for (let i = 0; i < 5; i++) {
+        let computerChoice = getComputerChoice();
+        let humanChoice = getHumanChoice();
+        playRound(humanChoice, computerChoice);
     }
 
-    else if (humanChoice == "Paper") {
-        if (computerChoice == "Rock") {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-            humanScore++;
+    if (humanScore > computerScore)
+        console.log(`You've won with a score of ${humanScore} to ${computerScore}.`);
+    else if (humanScore == computerScore)
+        console.log(`You've tied with a score of ${humanScore}`);
+    else
+        console.log(`You've won with a score of ${computerScore} to ${humanScore}.`)
+
+    // Goal: Function to play a round
+    // Steps: (1) Make choices case-insensitive (2) Decide winner
+    // using conditionals (3) Print proper message
+
+    function playRound(humanChoice, computerChoice) {
+        // Step 1:
+        humanChoice = humanChoice.slice(0,1).toUpperCase() + humanChoice.substr(1).toLowerCase();
+        computerChoice = computerChoice.slice(0,1).toUpperCase() + computerChoice.substr(1).toLowerCase();
+
+        // Step 2/3:
+        if (humanChoice == "Rock") {
+            if (computerChoice == "Rock") {
+                console.log("It's a tie!");
+            }
+
+            else if (computerChoice == "Paper") {
+                console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+                computerScore++;
+            }
+
+            else {
+                console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+                humanScore++;
+            }
         }
 
-        else if (computerChoice == "Paper") {
-            console.log("It's a tie!");
+        else if (humanChoice == "Paper") {
+            if (computerChoice == "Rock") {
+                console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+                humanScore++;
+            }
+
+            else if (computerChoice == "Paper") {
+                console.log("It's a tie!");
+            }
+
+            else {
+                console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+                computerScore++;
+            }
         }
 
-        else {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-            computerScore++;
-        }
-    }
+        else if (humanChoice == "Scissors") {
+            if (computerChoice == "Rock") {
+                console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+                computerScore++;
+            }
 
-    else if (humanChoice == "Scissors") {
-        if (computerChoice == "Rock") {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-            computerScore++;
-        }
+            else if (computerChoice == "Paper") {
+                console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+                humanScore++;
+            }
 
-        else if (computerChoice == "Paper") {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-            humanScore++;
-        }
-
-        else {
-            console.log("It's a tie!");
+            else {
+                console.log("It's a tie!");
+            }
         }
     }
 }
 
-
+playGame();
